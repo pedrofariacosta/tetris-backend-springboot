61fdbb81e59ec4f87d8dac84502dbbd449abf5ef
Pedro Arthur Faria Costa <pedrofaria356@gmail.com>
2025-10-27 15:02:28 -0300

    crição da entidade Jogador

---
+++ src/main/java/br/edu/ifsuldeminas/passos/tetris/model/Jogador.java
@@ -0,0 +1,50 @@
+package br.edu.ifsuldeminas.passos.tetris.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Jogador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeUsuario;
+    private String email;
+    private String senha;
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
