0b5a8be1f272630fe5886e14953f6f37d16b4938
Pedro Arthur Faria Costa <pedrofaria356@gmail.com>
2025-10-27 14:31:50 -0300

    commit inicial do projeto e configuração do H2

---
+++ src/test/java/br/edu/ifsuldeminas/passos/tetris/TetrisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsuldeminas.passos.tetris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
