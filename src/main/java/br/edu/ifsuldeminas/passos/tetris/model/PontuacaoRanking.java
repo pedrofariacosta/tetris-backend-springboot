987e13b36faac7b8e072265c1bf84480f967007b
Kaique <kaique.souza@alunos.ifsuldeminas.edu.br>
2025-10-27 17:53:10 -0300

    Cria a entidade PontuacaoRanking e relacionamento - Kaique

---
+++ src/main/java/br/edu/ifsuldeminas/passos/tetris/model/PontuacaoRanking.java
@@ -0,0 +1,56 @@
+package br.edu.ifsuldeminas.passos.tetris.model;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class PontuacaoRanking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int pontuacao;
+    private LocalDate dataRegistro;
+
+    @ManyToOne
+    private Jogador jogador;
+
+    //Getter e setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Jogador getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    public LocalDate getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(LocalDate dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+}
